0756b180522b299f6b3bc05c11913829bfabfaea
Bernardo Busatto <berbusatto@gmail.com>
2023-08-02 13:00:16 -0300

    CRUD de produto completo

--- App/controllers/PedidoController.php
+++ App/controllers/PedidoController.php
@@ -1,4 +1,32 @@
 <?php
-class PedidoController {
 
+use models\Pedido;
+
+class PedidoController {
+    private $pedidoModel;
+
+    public function __construct($conexao)
+    {
+        $this->pedidoModel = new Pedido($conexao);
+    }
+
+    public function listarPedidos()
+    {
+        return $this->pedidoModel->listarPedidos();
+    }
+
+    public function cadastrarPedido($dataPedido, $valorTotal, $cliente, $produtos, $quantidades)
+    {
+        $this->pedidoModel->cadastrarPedido($dataPedido, $valorTotal, $cliente, $produtos, $quantidades);
+    }
+
+    public function excluirPedido($pedidoId)
+    {
+        $this->pedidoModel->excluirPedido($pedidoId);
+    }
+
+    public function buscarPedidoPorId($pedidoId)
+    {
+        return $this->pedidoModel->buscarPedidoPorId($pedidoId);
+    }
 }
